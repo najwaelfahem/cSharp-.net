5ae45bd953026dac9c8ecbf7f1296a775570e27e
elfahemnajwa <elfahem13najwa@gmail.com>
2023-04-13 11:35:46 +0200

    finish

---
+++ week1/day2/core/WizardNinjaSamurai/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace WizardNinjaSamurai {
+    class Program {
+        static void Main (string[] args) {
+            Wizard xavier = new Wizard("Xavier");
+            Ninja elektra = new Ninja("Elektra");
+            Samurai logan = new Samurai("Logan");
+            logan.Attack(xavier);
+            xavier.Attack(elektra);
+            elektra.Attack(logan);
+            elektra.Steal(xavier);
+            logan.Meditate();
+            xavier.Heal(xavier);
+        }
+    }}
